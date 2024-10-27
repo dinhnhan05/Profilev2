5de138cf50b363056e67bd515e3afb191fed83ac
Nguyễn Đình Nhân <132151291+dinhnhan05@users.noreply.github.com>
2024-10-27 03:54:42 +0000

    jkos

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,11 @@
 // pages/_app.js
 import { Analytics } from '@vercel/analytics/react';
 
-import Script from 'next/script';
-
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            {/* Google Analytics */}
-            <Script
-                id="google-analytics"
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=G-9D7D9TJ5FM`} // Thay G-XXXXXXXXXX bằng mã theo dõi của bạn
-            />
-            <Script
-                id="google-analytics-init"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-                        gtag('config', 'G-9D7D9TJ5FM', {
-                          page_path: window.location.pathname,
-                        });
-                    `,
-                }}
-            />
-            {/* Vercel Analytics */}
-            <Analytics />
             <Component {...pageProps} />
+            <Analytics />
         </>
     );
 }
