36f97bcbce764ebecf6a2ba3c46db9b760e7edfe
Nguyễn Đình Nhân <132151291+dinhnhan05@users.noreply.github.com>
2024-11-08 22:37:59 +0700

    Update skills.tsx

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -20,13 +20,15 @@ const SkillsSection = () => {
         </ScrollAnimation>
       </div>
 
-      <Reavel>
+      
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:gap-4">
-        {TECHNOLOGIES.map((technology, index) => (
-          <TechDetail {...technology} key={index} />
-        ))}
+       <Reveal>
+  {TECHNOLOGIES.map((technology, index) => (
+    <TechDetail {...technology} key={index} />
+  ))}
+</Reveal>
       </div>
-        </Reavel>
+        
     </Container>
   );
 };
