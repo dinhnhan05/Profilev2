296cb027438c0ebd8a06f711fa54220b7813764d
Nguyễn Đình Nhân <132151291+dinhnhan05@users.noreply.github.com>
2024-11-10 12:39:58 +0700

    Delete src directory

--- src/components/general/nunito-font.tsx
+++
@@ -1,9 +0,0 @@
-import { Nunito } from "next/font/google";
-
-const nunito = Nunito({
-  subsets: ["latin"],
-  weight: "700",
-  display: "swap",
-});
-
-export default nunito;
--- src/components/general/space-grotesk-font.tsx
+++
@@ -1,9 +0,0 @@
-import { Space_Grotesk } from "next/font/google";
-
-const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  weight: "400",
-  display: "swap",
-});
-
-export default spaceGrotesk;
